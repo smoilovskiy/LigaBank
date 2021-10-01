13cac3c9f9b60226c01a52ac6157bdb90cbebad5
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-10-01 13:26:45 +0300

    add menu list

--- src/components/menuContainer/menuContainer.jsx
+++ src/components/menuContainer/menuContainer.jsx
@@ -1,16 +1,30 @@
 import { Fragment } from "react"
 
+
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+
+    <ListItem key={number.toString()} value={number} />
+  );
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4];
+
+
 const MenuContainer = () => {
   return (
     <Fragment>
-      <div>
-        <ul>
-          <li>item1</li>
-          <li>item2</li>
-          <li>item3</li>
-          <li>item4</li>
-        </ul>
-      </div>
+      <NumberList numbers={numbers} />
     </Fragment>
   );
 };
