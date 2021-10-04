3b9fe86e5012d0d5294f00ee792fd928e64aebcc
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-10-04 11:54:55 +0300

    add BEM styles for slides

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -12,32 +12,32 @@ export default () => (
   >
 
     <div className='slide1'>
-      <div className='slide1-inner'>
+      <div className='slide1__inner'>
 
-        <div className='slide1-img1-container'>
-          <img className='slide1-img1' alt="white-card" src="img/white-card.png" />
+        <div className='slide1__container--img1'>
+          <img className='slide1__img1' alt="white-card" src="img/white-card.png" />
         </div>
-        <div className='slide1-img2-container'>
-          <img className='slide1-img2' alt="black-card" src="img/black-card.png" />
+        <div className='slide1__container--img2'>
+          <img className='slide1__img2' alt="black-card" src="img/black-card.png" />
         </div>
 
-        <p className='slide1-title'>Лига Банк</p>
-        <p className='slide1-subtitle'>Кредиты на любой случай</p>
-        <a className='slide1-link' href='##'>Рассчитать кредит</a>
+        <p className='slide1__title'>Лига Банк</p>
+        <p className='slide1__subtitle'>Кредиты на любой случай</p>
+        <a className='slide1__link' href='##'>Рассчитать кредит</a>
       </div>
     </div>
 
     <div className='slide2'>
       <img alt="slide2" src="img/slide2-bg.jpg" />
-      <p className='slide2-title'>Лига Банк</p>
-      <p className='slide2-subtitle'>Ваша уверенность в завтрашнем дне</p>
+      <p className='slide2__title'>Лига Банк</p>
+      <p className='slide2__subtitle'>Ваша уверенность в завтрашнем дне</p>
     </div>
 
     <div className='slide3'>
       <img alt="slide3" src="img/slide3-bg.jpg" />
-      <p className='slide3-title'>Лига Банк</p>
-      <p className='slide3-subtitle'>Всегда рядом</p>
-      <a className='slide3-link' href='##'>Найти отделение</a>
+      <p className='slide3__title'>Лига Банк</p>
+      <p className='slide3__subtitle'>Всегда рядом</p>
+      <a className='slide3__link' href='##'>Найти отделение</a>
     </div>
   </Carousel>
 );
