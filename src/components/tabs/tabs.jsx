dc485b6dc0e1cc1ad069ed0ad5bd8b4bf70cb79e
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-10-08 19:37:16 +0300

    add tab render function

--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -1,87 +1,28 @@
 import React, { useState } from 'react';
 import { Fragment } from 'react';
+import RenderTabContent from './view/renderTabContent';
 import './tabs.scss'
 
-const tabContent = [<div className='tab-container'>
-  <p className='tab__text-big line1'>Вклады Лига Банка – это выгодная</p>
-  <p className='tab__text-big line2'>инвестиция в свое будущее</p>
-  <img className='checkmark-icon checkmark1' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line3'>Проценты по вкладам до 7%</p>
-  <img className='checkmark-icon checkmark2' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line4'>Разнообразные условия</p>
-  <img className='checkmark-icon checkmark3' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line5'>Возможность ежемесячной капитализации</p>
-  <p className='tab__text-small line6'>или вывод процентов на банковскую карту</p>
-  <a className='tab__link-bttn' href='##'>Узнать подробнее</a>
-
-  <img className='tab-img' src='./img/piggybank.jpg' alt='piggybank-img' />
-</div>
-
-  , <div className='tab-container'>
-  <p className='tab__text-big line1'>Лига Банк выдает кредиты под</p>
-  <p className='tab__text-big line2'>любые цели</p>
-  <img className='checkmark-icon checkmark1' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line3'>Ипотечный кредит</p>
-  <img className='checkmark-icon checkmark2' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line4'>Автокредит</p>
-  <img className='checkmark-icon checkmark3' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line5'>Потребительский кредит</p>
-
-  <p className='tab__text-small line7'>Рассчитайте ежемесячный платеж</p>
-  <p className='tab__text-small line8'>и ставку по кредиту воспользовавшись</p>
-  <p className='tab__text-small line9'>нашим</p>
-  <a className='tab__link' href='##'>кредитным калькулятором</a>
-  <img className='tab-img' src='./img/car.jpg' alt='car-img' />
-</div>
-  , <div className='tab-container'>
-  <p className='tab__text-big line1'>Лига Страхование — застрахуем все</p>
-  <p className='tab__text-big line2'>что захотите</p>
-  <img className='checkmark-icon checkmark1' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line3'>Автомобильное страхование</p>
-  <img className='checkmark-icon checkmark2' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line4'>Страхование жизни и здоровья</p>
-  <img className='checkmark-icon checkmark3' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line5'>Страхование недвижимости</p>
-  <a className='tab__link-bttn' href='##'>Узнать подробнее</a>
-  <img className='tab-img' src='./img/lock.jpg' alt='lock-img' />
-</div>
-  , <div className='tab-container'>
-  <p className='tab__text-big line1'>Лига Банк — это огромное количество</p>
-  <p className='tab__text-big line2'>онлайн-сервисов для вашего удобства</p>
-  <img className='checkmark-icon checkmark1' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line3'>Мобильный банк,</p>
-  <p className='tab__text-small line3-1'>который всегда под рукой</p>
-  <img className='checkmark-icon checkmark3' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
-  <p className='tab__text-small line5'>Приложение Лига-проездной позволит</p>
-  <p className='tab__text-small line6'>вам оплачивать билеты по всему миру</p>
-  <a className='tab__link-bttn' href='##'>Узнать подробнее</a>
-  <img className='tab-img' src='./img/phone.jpg' alt='phone-img' />
-</div>
-]
-
 const tabsItems = [
-  { key: 1, title: 'Вклады', imgUrl: './img/icon/deposits.svg', imgAlt: 'deposits-icon', content: tabContent[0] },
-  { key: 2, title: 'Кредиты', imgUrl: './img/icon/credits.svg', imgAlt: 'credits-icon', content: tabContent[1] },
-  { key: 3, title: 'Страхование', imgUrl: './img/icon/insurance.svg', imgAlt: 'insurance-icon', content: tabContent[2] },
-  { key: 4, title: 'Онлайн-сервисы', imgUrl: './img/icon/online-services.svg', imgAlt: 'onlineServices-icon', content: tabContent[3] },
+  { key: 1, title: 'Вклады', imgUrl: './img/icon/deposits.svg', imgAlt: 'deposits-icon', id: 0 },
+  { key: 2, title: 'Кредиты', imgUrl: './img/icon/credits.svg', imgAlt: 'credits-icon', id: 1 },
+  { key: 3, title: 'Страхование', imgUrl: './img/icon/insurance.svg', imgAlt: 'insurance-icon', id: 2 },
+  { key: 4, title: 'Онлайн-сервисы', imgUrl: './img/icon/online-services.svg', imgAlt: 'onlineServices-icon', id: 3 },
 ];
-
 
 function Tabs() {
 
   const [active, setActive] = useState(tabsItems[0].title);
-  const [activeTab, setActiveTab] = useState(tabContent[0]);
-
-
+  const [activeTab, setActiveTab] = useState(0);
   const activeTitleBg = { 'background': '#F6F7FF' };
 
   const activeTitle = tabsItems.map(tabsItem => (
 
     <div className='tab-select' key={tabsItem.key}
-      onClick={() => setActive(tabsItem.title)} style={tabsItem.title === active ? activeTitleBg : null}>
+      onClick={() => {setActive(tabsItem.title); setActiveTab(tabsItem.id)}} style={tabsItem.title === active ? activeTitleBg : null}>
       <div className='tab-select__title'>
-        <img className='tab-select__title-img' src={tabsItem.imgUrl} alt={tabsItem.imgAlt} />
-        <span className='tab-select__title-text'>{tabsItem.title}</span>
+        
+        <span className='tab-select__title-text'> <img className='tab-select__title-img' src={tabsItem.imgUrl} alt={tabsItem.imgAlt} />{tabsItem.title}</span>
       </div>
     </div>
   ));
@@ -92,12 +33,10 @@ function Tabs() {
       <div className='tabs-container'>
         <div className='tabs-selector'>
           {activeTitle}
+
         </div>
 
-        {/* 
-        <RenderTabContent tabContent={tabContent}/> */}
-
-        {tabsItems.find(tab => tab.title === active).content}
+        <RenderTabContent activeTab={activeTab}/>
 
       </div>
     </Fragment>
--- src/components/tabs/view/renderTabContent.jsx
+++ src/components/tabs/view/renderTabContent.jsx
@@ -1,7 +1,66 @@
+import React from "react";
+import '../tabs.scss';
 
+
+const tabContent = [<div className='tab-container'>
+  <p className='tab__text-big line1'>Вклады Лига Банка – это выгодная</p>
+  <p className='tab__text-big line2'>инвестиция в свое будущее</p>
+  <img className='checkmark-icon checkmark1' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line3'>Проценты по вкладам до 7%</p>
+  <img className='checkmark-icon checkmark2' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line4'>Разнообразные условия</p>
+  <img className='checkmark-icon checkmark3' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line5'>Возможность ежемесячной капитализации</p>
+  <p className='tab__text-small line6'>или вывод процентов на банковскую карту</p>
+  <a className='tab__link-bttn' href='##'>Узнать подробнее</a>
+
+  <img className='tab-img' src='./img/piggybank.jpg' alt='piggybank-img' />
+</div>
+
+  , <div className='tab-container'>
+  <p className='tab__text-big line1'>Лига Банк выдает кредиты под</p>
+  <p className='tab__text-big line2'>любые цели</p>
+  <img className='checkmark-icon checkmark1' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line3'>Ипотечный кредит</p>
+  <img className='checkmark-icon checkmark2' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line4'>Автокредит</p>
+  <img className='checkmark-icon checkmark3' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line5'>Потребительский кредит</p>
+
+  <p className='tab__text-small line7'>Рассчитайте ежемесячный платеж</p>
+  <p className='tab__text-small line8'>и ставку по кредиту воспользовавшись</p>
+  <p className='tab__text-small line9'>нашим</p>
+  <a className='tab__link' href='##'>кредитным калькулятором</a>
+  <img className='tab-img' src='./img/car.jpg' alt='car-img' />
+</div>
+  , <div className='tab-container'>
+  <p className='tab__text-big line1'>Лига Страхование — застрахуем все</p>
+  <p className='tab__text-big line2'>что захотите</p>
+  <img className='checkmark-icon checkmark1' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line3'>Автомобильное страхование</p>
+  <img className='checkmark-icon checkmark2' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line4'>Страхование жизни и здоровья</p>
+  <img className='checkmark-icon checkmark3' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line5'>Страхование недвижимости</p>
+  <a className='tab__link-bttn' href='##'>Узнать подробнее</a>
+  <img className='tab-img' src='./img/lock.jpg' alt='lock-img' />
+</div>
+  , <div className='tab-container'>
+  <p className='tab__text-big line1'>Лига Банк — это огромное количество</p>
+  <p className='tab__text-big line2'>онлайн-сервисов для вашего удобства</p>
+  <img className='checkmark-icon checkmark1' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line3'>Мобильный банк,</p>
+  <p className='tab__text-small line3-1'>который всегда под рукой</p>
+  <img className='checkmark-icon checkmark3' src='./img/icon/checkmark.svg' alt='checkmark-icon' />
+  <p className='tab__text-small line5'>Приложение Лига-проездной позволит</p>
+  <p className='tab__text-small line6'>вам оплачивать билеты по всему миру</p>
+  <a className='tab__link-bttn' href='##'>Узнать подробнее</a>
+  <img className='tab-img' src='./img/phone.jpg' alt='phone-img' />
+</div>
+]
 
 function RenderTabContent(props) {
-  
+  return tabContent[props.activeTab]
 }
 
 export default RenderTabContent;
