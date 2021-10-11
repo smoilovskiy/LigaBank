cd0e6401bdd1afa27fae0d6e62c3ad66f1c30fd9
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-10-11 17:59:09 +0300

    add positions for cities on map

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -5,7 +5,7 @@ import 'leaflet/dist/leaflet.css';
 import L from "leaflet";
 import './map.scss';
 
-const position = [51.505, -0.09]
+const position = [56.70, 60.35];
 
 const ligaBankIcon = L.icon({
   iconUrl: '/img/icon/marker.svg',
@@ -14,10 +14,11 @@ const ligaBankIcon = L.icon({
 });
 
 const markers = [
-  { key: 1, position: [51.505, -0.09], popupText: 'Текст1' },
-  { key: 2, position: [51.515, -0.09], popupText: 'Текст2' },
-  { key: 3, position: [51.525, -0.09], popupText: 'Текст3' },
-  { key: 4, position: [51.535, -0.09], popupText: 'Текст4' },
+  { key: 1, position: [57.09, 65.32], popupText: 'Текст1' },
+  { key: 2, position: [55.45, 37.36], popupText: 'Текст2' },
+  { key: 3, position: [51.53, 46.00], popupText: 'Текст3' },
+  { key: 4, position: [54.59, 73.22], popupText: 'Текст4' },
+  { key: 5, position: [55.57, 49.70], popupText: 'Текст5' },
 ];
 
 function MapRender() {
@@ -26,7 +27,7 @@ function MapRender() {
       
       <div className='map-container'>
       <h2 className='bank-branches'>Отделения Лига Банка</h2>
-        <MapContainer center={position} zoom={12} scrollWheelZoom={false} style={{ height: "462px", width: "100%" }}>
+        <MapContainer center={position} zoom={5} scrollWheelZoom={false} style={{ height: "462px", width: "100%" }}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
