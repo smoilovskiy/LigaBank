ef737b0a432a5caef77362782a6f87b9d100c405
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-10-13 22:45:03 +0300

    add drop down menu without select-options

--- src/components/calculator/calculator.jsx
+++ src/components/calculator/calculator.jsx
@@ -1,21 +1,49 @@
+import React from "react";
 import { Fragment } from "react"
 
 import './calculator.scss'
 
 
+function renderDropDown() {
+  return React.cloneElement(this.doRender(), {
+    style: {border: '1px solid red'}
+});
+  // credit-selector__drop-down.
+}
+
+
 function Calculator() {
   const step1 = 'Шаг 1. Цель кредита';
+
+
+
+
 
   return (
     <Fragment>
       <div className='calculator__container'>
         <h3 className='calculator__title'>Кредитный калькулятор</h3>
         <p className='calculator__steps-text'>{step1}</p>
-        <select className='step-selector'>
+
+        <div className='credit-selector'>
+          <p className='credit-selector__title'>Выберите цель кредита</p>
+          <img className='credit-selector__arrow' src='./img/icon/arrow-down.svg' alt='credit-selector-arrow' onClick={renderDropDown}></img>
+        </div>
+        <div className='credit-selector__drop-down'>
+          <div className='mortgage-credit-lending'>
+
+          </div>
+          <div className='car-credit-lending'>
+
+          </div>
+        </div>
+
+
+        {/* <select className='step-selector'>
           <option className='step-option-default' disabled selected>Выберите цель кредита</option>
           <option className='step-option' value="mortgage-credit-lending">Ипотечное кредитование</option>
           <option className='step-option' value="car-credit-lending">Автомобильное кредитование</option>
-        </select>
+        </select> */}
       </div>
     </Fragment>
   );
