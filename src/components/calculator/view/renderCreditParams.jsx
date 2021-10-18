a9c8e8b5c127b23e4ba45b136a8b4b275f50ec2c
Rostislav Smoilovskiy <smoilovskiy@gmail.com>
2021-10-18 22:05:05 +0300

    add apply form

--- src/components/calculator/view/renderCreditParams.jsx
+++ src/components/calculator/view/renderCreditParams.jsx
@@ -1,4 +1,5 @@
 import React, { useState, createRef } from 'react';
+
 import '../calculator.scss';
 
 const CREDIT_COST_INIT = 2000000;
@@ -8,22 +9,38 @@ function RenderCreditParams(props) {
   const step2 = 'Шаг 2. Введите параметры кредита';
   const realEstateCost = 'Стоимость недвижимости';
   const carCost = 'Стоимость автомобиля';
-
   const [creditCost, setCreditCost] = useState(CREDIT_COST_INIT);
-  // const [input, setInput] = useState('');
+  const [active, setApplyFormActive] = useState({ display: "none" });
+  const [initialFeeValue, setInitialFeeValue] = useState(10);
+  const [loanTermsValue, setLoanTermsValue] = useState(5);
 
+
+  const handleInitialFeeChange = e => {
+    setInitialFeeValue(e.target.value);
+  };
+
+  const handleLoanTermsChange = e => {
+    setLoanTermsValue(e.target.value);
+  };
 
   let inputText = createRef();
 
-  // function handleInput() {
-  //   inputText.current.value = creditCost;
-  //   console.log(inputText.current.value);
+  function applyFormActive() {
+    setApplyFormActive({ display: "block" })
+  }
+
+  // function applyForm() {
+  //   alert([document.forms["apply-form"].elements["initial-fee"].value,
+  //   document.forms["apply-form"].elements["order-number"].value,
+  //   document.forms["apply-form"].elements["credit-target"].value])
+
+
+  //   return false
   // }
+
 
   function handleCreditCostInput() {
     setCreditCost(+inputText.current.value)
-    // console.log(inputText.current.value)
-
   }
 
   console.log(creditCost)
@@ -35,6 +52,7 @@ function RenderCreditParams(props) {
   function increaseCreditCost() {
     setCreditCost(creditCost + 100000)
   }
+
 
   // const [sliderProps, setSliderProps] = useState({
   //   min: 0,
@@ -79,30 +97,108 @@ function RenderCreditParams(props) {
       <div className='credit-selector-condensed'>
 
       </div>
-      {/* <input
-          type="range"
-          value={sliderVal}
-          {...sliderProps}
-          className={`slider ${classes}`}
-          id="myRange"
-          onChange={changeCallback}
-          onMouseDown={() => setMouseState("down")}
-          onMouseUp={() => setMouseState("up")}
-        /> */}
+
+      <input
+        type="range"
+        className="range-slider"
+        min={10}
+        step={5}
+        // max={max}
+        value={initialFeeValue}
+        onChange={handleInitialFeeChange}
+      />
 
       <p className='credit-params-subtitle'>Срок кредитования</p>
       <div className='credit-selector-condensed'>
 
       </div>
+
+      <input
+        type="range"
+        className="range-slider"
+        min={5}
+        max={30}
+        value={loanTermsValue}
+        onChange={handleLoanTermsChange}
+      />
+
+      <div className='chekbox-container'>
+        <input
+          type="checkbox"
+          className="credit-params-checkbox"
+        // min={min}
+        // max={max}
+        // value={credit}
+        // onChange={changeCallback}
+        />
+        <p className="checkbox-container-text">Использовать материнский капитал</p>
+      </div>
+
       <div className='our-offer'>
         <div className='our-offer__inner'>
-          <p className='our-offer__title'>Наше предложение</p>
-          <p className='amount-value'>{creditCost.toLocaleString('ru')} рублей</p>
-          <p className='amount-subtitle'>Сумма ипотеки</p>
-          <p className='amount-value'>{(creditCost/5).toLocaleString('ru')} рублей</p>
-          <p className='amount-subtitle'>Ежемесячный платеж</p>
+          <div className='our-offer__inner-left'>
+            <p className='our-offer__title'>Наше предложение</p>
+            <p className='amount-value'>{creditCost.toLocaleString('ru')} рублей</p>
+            <p className='amount-subtitle'>Сумма ипотеки</p>
+            <p className='amount-value'>{(creditCost / 5).toLocaleString('ru')} рублей</p>
+            <p className='amount-subtitle'>Ежемесячный платеж</p>
+          </div>
+
+          <div className='our-offer__inner-right'>
+            <p className='amount-value'>{initialFeeValue} %</p>
+            <p className='amount-subtitle'>Процентная ставка</p>
+            <p className='amount-value'>{(creditCost / 40).toLocaleString('ru')} рублей</p>
+            <p className='amount-subtitle'>Необходимый доход</p>
+          </div>
+
+          <a className='our-offer__link-bttn' href='##' onClick={applyFormActive}>Оформить заявку</a>
+
         </div>
       </div>
+      <div className='apply-form-container' style={active}>
+
+        <div className='apply-form__inner'>
+          <div className='apply-form__title-container'>
+            <p className='apply-form__title'>Шаг 3. Оформление заявки</p>
+          </div>
+          <form id='apply-form'>
+            <div className='apply-form__data'>
+              <p className='apply-form__data-title'>Номер заявки</p>
+              <label className='apply-form__data-value'>{creditCost}</label>
+              <input type="hidden" name='order-number' value={creditCost / 1000} />
+            </div>
+            <div className='apply-form__data'>
+              <p className='apply-form__data-title'>Цель кредита</p>
+              <label className='apply-form__data-value'>{creditCost}</label>
+              <input type="hidden" name='credit-target' value={creditCost / 56} />
+            </div>
+            <div className='apply-form__data'>
+              <p className='apply-form__data-title'>Стоимость недвижимости</p>
+              <label className='apply-form__data-value'>{creditCost} рублей</label>
+              <input type="hidden" name='credit-cost' value={creditCost} />
+            </div>
+            <div className='apply-form__data'>
+              <p className='apply-form__data-title'>Первоначальный взнос</p>
+              <label className='apply-form__data-value'>{creditCost} рублей</label>
+              <input type="hidden" name='initial-fee' value={creditCost} />
+            </div>
+            <div className='apply-form__data'>
+              <p className='apply-form__data-title'>Срок кредитования</p>
+              <label className='apply-form__data-value'>{loanTermsValue} лет</label>
+              <input type="hidden" name='loan-terms' value={loanTermsValue} />
+            </div>
+
+            <input className='apply-form__customer-name' type="text" name='customer-name' placeholder='ФИО' />
+            <div className='apply-form__customer-contacts'>
+              <input className='apply-form__customer-phone' type="tel" name='customer-phone' placeholder='Телефон' />
+              <input className='apply-form__customer-email' type="mailto" name='customer-email' placeholder='E-mail' />
+            </div>
+
+            <button type='submit' className='apply-form__submit-button' onClick>Отправить</button>
+          </form>
+        </div>
+      </div>
+
     </div>
 
     ,
@@ -126,4 +222,4 @@ function RenderCreditParams(props) {
   return creditParams[props.content]
 }
 
-export default RenderCreditParams;
+export default RenderCreditParams
